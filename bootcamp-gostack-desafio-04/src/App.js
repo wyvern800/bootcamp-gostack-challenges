315fd1878773320c28db6079144b48279c4af537
wyvern800 <matheusguiferreira@hotmail.com>
2019-10-23 19:19:13 -0300

    changes: App

--- bootcamp-gostack-desafio-04/src/App.js
+++ bootcamp-gostack-desafio-04/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import "./components/Header";
+import Header from "./components/Header";
+import PostList from "./components/PostList";
 
 function App() {
   return <Header />;
