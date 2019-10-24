28dde315e8222b4cae6039fb3c913157dc8b4e49
wyvern800 <matheusguiferreira@hotmail.com>
2019-10-24 10:48:13 -0300

    add: Post component

--- bootcamp-gostack-desafio-04/src/components/Post.js
+++ bootcamp-gostack-desafio-04/src/components/Post.js
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 
+import Comments from "../components/Comments";
+
 // import { Container } from './styles';
 
-class Post extends Component {
-  componentDidMount() {
-    console.log("Post carregado");
-  }
-
-  render() {
-    return <div>Bike</div>;
-  }
+function Post({ data }) {
+  return (
+    <li className="post" id={data.id}>
+      <div className="post_box">
+        <div className="post_author_info">
+          <img
+            className="post_author_img"
+            src={data.author.avatar}
+            alt="Profile"
+          />
+          <ul>
+            <li>
+              <span className="post_author_name">
+                {data.author && data.author.name}
+              </span>
+            </li>
+            <li>
+              <span className="post_date">{data.date}</span>
+            </li>
+          </ul>
+        </div>
+        <p className="post_content">{data.content}</p>
+        <hr className="post_hr" />
+        {data.comments.map(comment => (
+          <Comments key={comment.id} comments={comment} />
+        ))}
+      </div>
+    </li>
+  );
 }
 
 export default Post;
