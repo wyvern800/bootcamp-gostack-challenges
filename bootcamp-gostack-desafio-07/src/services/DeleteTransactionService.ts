5cad45e5aaded43fd9bf12924899965810130a87
wyvern800 <matheusguiferreira@hotmail.com>
2021-08-18 00:47:13 -0300

    add: project files

---
+++ bootcamp-gostack-desafio-07/src/services/DeleteTransactionService.ts
@@ -0,0 +1,23 @@
+// import AppError from '../errors/AppError';
+
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
+
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+class DeleteTransactionService {
+  public async execute(id: string): Promise<void> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('A transaction with that id was not found!');
+    }
+
+    await transactionRepository.remove(transaction);
+  }
+}
+
+export default DeleteTransactionService;
